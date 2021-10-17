da8d867a8fdfa94e6b84014a887c599fac7aaa03
yrrah <yrrah7@gmail.com>
2021-10-17 16:19:51 -0400

    refactor to organize code

--- simulator/end_to_end_example.py
+++ simulator/end_to_end_example.py
@@ -10,19 +10,37 @@ import matplotlib.pyplot as plt
 from simulator.front_end.src.front_end.routing.simple_algo import simple_routing
 
 
-def main():
-    num_domains = 10
-    max_files = 10
-    hash_file_dir = './hash_files/'
-    domains = [KeyValueStore() for _ in range(num_domains)]
+def plot_distribution(domain_stats: [float]):
+    x_pos = [i for i, _ in enumerate(domain_stats)]
+    fig, ax = plt.subplots()
+    ax.bar(x_pos, domain_stats, color='green')
+    ax.set_xlabel("Domains")
+    ax.set_ylabel("Percent of Total Fingerprints Assigned")
+    ax.set_title("Fingerprint Distribution")
+    fig.savefig('end_to_end_example_counts.png')
+    plt.close(fig)
 
+
+def plot_compression_ratio(domain_stats: [float]):
+    x_pos = [i for i, _ in enumerate(domain_stats)]
+    fig, ax = plt.subplots()
+    ax.bar(x_pos, domain_stats, color='green')
+    ax.set_xlabel("Domains")
+    ax.set_ylabel("Compression Ratio")
+    ax.set_title("Stored Fingerprints/Total Fingerprints per Domain")
+    fig.savefig('end_to_end_example_dedup.png')
+    plt.close(fig)
+
+
+def locally_running_example(num_domains: int = 10, max_input_files: int = 10, hash_file_dir: str = './hash_files/'):
+    domains = [KeyValueStore() for _ in range(num_domains)]
     hash_files = [file for file in os.listdir(hash_file_dir) if file.endswith(".hash.anon")]
     num_files = len(hash_files)
 
     domain_total_received = [0 for _ in range(num_domains)]
     total_overall = 0
 
-    for filename in tqdm(hash_files[:min(max_files, num_files)]):
+    for filename in tqdm(hash_files[:min(max_input_files, num_files)]):
         hash_file = HashFile(hash_file_dir + filename)
         for region in create_fixed_regions(hash_file, size_mib=1):
             destination = simple_routing(region, num_domains)
@@ -30,6 +48,7 @@ def main():
             domain_total_received[destination] += len(region.fingerprints)
             total_overall += len(region.fingerprints)
 
+    # collect statistics
     dedup = [0.0 for _ in range(num_domains)]
     counts = [0.0 for _ in range(num_domains)]
     for i in range(num_domains):
@@ -37,25 +56,12 @@ def main():
             dedup[i] = domains[i].get_current_count()/domain_total_received[i]
         counts[i] = domain_total_received[i]/total_overall
 
-    # counts = [domain.get_current_count()/total_overall for domain in domains]
+    plot_distribution(counts)
+    plot_compression_ratio(dedup)
 
-    x_pos = [i for i, _ in enumerate(counts)]
 
-    fig, ax = plt.subplots()
-    ax.bar(x_pos, counts, color='green')
-    ax.set_xlabel("Domains")
-    ax.set_ylabel("Percent of Total Fingerprints Assigned")
-    ax.set_title("Fingerprint Distribution")
-    fig.savefig('end_to_end_example_counts.png')
-    plt.close(fig)
-
-    fig, ax = plt.subplots()
-    ax.bar(x_pos, dedup, color='green')
-    ax.set_xlabel("Domains")
-    ax.set_ylabel("Compression Ratio")
-    ax.set_title("Stored Fingerprints/Total Fingerprints per Domain")
-    fig.savefig('end_to_end_example_dedup.png')
-    plt.close(fig)
+def main():
+    locally_running_example()
 
 
 if __name__ == "__main__":
