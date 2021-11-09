775441ff63445c2a5793a1a25cd67548ebcd2ac1
yrrah <yrrah7@gmail.com>
2021-11-09 03:54:01 -0500

    clean up duplicate code

--- simulator/back_end/src/back_end/grpc/hello_world_demo/greeter_client.py
+++ simulator/back_end/src/back_end/grpc/hello_world_demo/greeter_client.py
@@ -18,7 +18,6 @@ from __future__ import print_function
 import logging
 import os
 import grpc
-import pyrocksdb
 
 from back_end.grpc.hello_world_demo import helloworld_pb2, helloworld_pb2_grpc
 
@@ -32,33 +31,6 @@ def run():
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
-    db_test()
-
-
-def db_test():
-    db = pyrocksdb.DB()
-    opts = pyrocksdb.Options()
-    # for multi-thread
-    # opts.IncreaseParallelism()
-    # opts.OptimizeLevelStyleCompaction()
-    opts.create_if_missing = True
-    s = db.open(opts, 'db_files')
-    print(s.code())
-    assert (s.ok())
-    # put
-    opts = pyrocksdb.WriteOptions()
-    s = db.put(opts, b"key1", b"value1")
-    assert (s.ok())
-    # get
-    opts = pyrocksdb.ReadOptions()
-    blob = db.get(opts, b"key1")
-    print(blob.data)  # b"value1"
-    print(blob.status.ok())  # true
-    # delete
-    opts = pyrocksdb.WriteOptions()
-    s = db.delete(opts, b"key1")
-    assert (s.ok())
-    db.close()
 
 
 if __name__ == '__main__':
