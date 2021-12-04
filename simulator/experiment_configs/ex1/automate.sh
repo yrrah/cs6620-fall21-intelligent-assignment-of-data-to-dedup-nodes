24e829164df2a07d016fea1be09c0955bbad42b9
yrrah <yrrah7@gmail.com>
2021-12-04 01:36:32 -0500

    add ex1 configs

--- simulator/experiment_configs/ex1/automate.sh
+++ simulator/experiment_configs/ex1/automate.sh
@@ -143,7 +143,7 @@ print_all_params () {
 }
 
 exec < run_combinations.tsv
-for i in {1..1}; do read skip_these_lines; done;
+for i in {1..27}; do read skip_these_lines; done;
 while IFS=$'\t' read -r run_num dedup_domains num_pods region_algo region_size min_region max_region bitmask_size assign_algo dataset results_file
 do
   kill_old_pods
@@ -174,7 +174,7 @@ do
   done
 
   ## stop early
-  if [ "$run_num" -eq 110 ]; then
+  if [ "$run_num" -eq 136 ]; then
     break
   fi
 done
