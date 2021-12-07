6f80cbdefb09bb30efa76ba0e237b777dcfc6391
yrrah <yrrah7@gmail.com>
2021-12-07 07:00:58 -0500

    add summary plots

--- simulator/results/parse_summary.py
+++ simulator/results/parse_summary.py
@@ -3,17 +3,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def plot_reversed(ax):
+def plot_reversed(ax, save, show, title):
     # ax.set_xticklabels(df.run_combo, rotation = 90)
     ax.yaxis.tick_right()
     ax.yaxis.set_label_position("right")
     ax.invert_xaxis()
     plt.gcf().subplots_adjust(right=0.5)
     # plt.tight_layout()
-    plt.show()
+    if save:
+        plt.savefig(f'{title}.svg', format='svg')
+    if show:
+        plt.show()
 
 
-def plot_times(df, dataset: int):
+def plot_times(df, dataset: int, save, show):
     df['run_combo'] = df['run_combo'].str[20:]
     ax = df[(df['dataset'] == dataset) & (df['domains_per_pod'] == 32)] \
         .sort_values(['region_size', 'duration'], ascending=False) \
@@ -21,10 +24,10 @@ def plot_times(df, dataset: int):
     ax.set_title(f'Dataset {dataset} Processing Time')
     ax.set_xlabel('Minutes')
     ax.get_legend().remove()
-    plot_reversed(ax)
+    plot_reversed(ax, save, show, f'dataset{dataset}_duration')
 
 
-def plot_regions(df):
+def plot_regions(df, save, show):
     df['run_combo'] = 'Data' + df['dataset'].astype(str) + ', ' + df['region_size'].astype(str) + 'MB ' + df['region_algo']
     ax = df[(df['assign_algo'] == 'MIN_FINGERPRINT') & (df['domains_per_pod'] == 32)] \
         .sort_values(['mean_region_bytes'], ascending=False) \
@@ -36,17 +39,17 @@ def plot_regions(df):
     ax.axvline(x=8*1024*1024)
     ax.set_xticks([n*1024*1024 for n in range(0, 13, 2)])
     ax.set_xticklabels([f'{n}MB' for n in range(0, 13, 2)])
-    plot_reversed(ax)
+    plot_reversed(ax, save, show, 'region_creation_stats')
 
 
-def plot_dedup(df, dataset: int):
+def plot_dedup(df, dataset: int, save, show):
     ax = df[(df['dataset'] == dataset)] \
         .sort_values(['overall_dedup'], ascending=False) \
         .plot.barh(x='run_combo', y='overall_dedup', figsize=(12, 20))
     ax.set_title(f'Dataset {dataset} Overall De-duplication')
     # ax.set_xlabel('Minutes')
     # ax.get_legend().remove()
-    plot_reversed(ax)
+    plot_reversed(ax, save, show, f'dataset{dataset}_deduplication')
 
 
 def plot_skew_scatter(df, dataset: int, column: str, cv=False):
@@ -74,23 +77,29 @@ def plot_skew_scatter(df, dataset: int, column: str, cv=False):
     return ax
 
 
-def plot_pod_skew_logical_scatter(df, dataset: int):
+def plot_pod_skew_logical_scatter(df, dataset: int, save, show):
     ax = plot_skew_scatter(df, dataset, 'pod_logical')
     ax.set_title(f'Dataset {dataset} Overall Pod Logical Skew')
     ax.set_xlabel('Overall Deduplication (----> More is Better)')
     ax.set_ylabel('(<---- Less is Better) Standard Deviation of Total Pod Logical Bytes')
-    plt.show()
+    if save:
+        plt.savefig(f'dataset{dataset}_pod_skew_logical_scatter.svg', format='svg')
+    if show:
+        plt.show()
 
 
-def plot_pod_skew_physical_scatter(df, dataset: int):
+def plot_pod_skew_physical_scatter(df, dataset: int, save, show):
     ax = plot_skew_scatter(df, dataset, 'pod_phys')
     ax.set_title(f'Dataset {dataset} Overall Pod Physical Skew')
     ax.set_xlabel('Overall Deduplication (----> More is Better)')
     ax.set_ylabel('(<---- Less is Better) Standard Deviation of Total Pod Physical Bytes')
-    plt.show()
+    if save:
+        plt.savefig(f'dataset{dataset}_pod_skew_physical_scatter.svg', format='svg')
+    if show:
+        plt.show()
 
 
-def summary_plots(file_name: str):
+def summary_plots(file_name: str, save, show):
     df = pd.read_csv(file_name)
     df['q_learning'] = np.where(df['q_learning'] == 'True', 'w/Q', '')
     df['q_penalty'] = np.where(df['q_penalty'] == 'True', '+penalty', '')
@@ -99,24 +108,23 @@ def summary_plots(file_name: str):
         str) + ' domains, ' + df['region_size'].astype(str) + 'MB ' + df['region_algo'] + ', ' + \
         df['assign_algo'] + ' ' + df['q_learning'] + df['eps'] + df['q_penalty']
 
-    # plot_times(df.copy(), dataset=1)
-    # plot_times(df.copy(), dataset=2)
-    # plot_times(df.copy(), dataset=3)
-    # plot_regions(df.copy())
-    # plot_dedup(df.copy(), dataset=1)
-    # plot_dedup(df.copy(), dataset=2)
-    # plot_dedup(df.copy(), dataset=3)
-    # plot_pod_skew(df.copy(), dataset=1)
-    plot_pod_skew_logical_scatter(df.copy(), dataset=1)
-    plot_pod_skew_logical_scatter(df.copy(), dataset=2)
-    plot_pod_skew_logical_scatter(df.copy(), dataset=3)
-    plot_pod_skew_physical_scatter(df.copy(), dataset=1)
-    plot_pod_skew_physical_scatter(df.copy(), dataset=2)
-    plot_pod_skew_physical_scatter(df.copy(), dataset=3)
+    plot_times(df.copy(), 1, save, show)
+    plot_times(df.copy(), 2, save, show)
+    plot_times(df.copy(), 3, save, show)
+    plot_regions(df.copy(), save, show)
+    plot_dedup(df.copy(), 1, save, show)
+    plot_dedup(df.copy(), 2, save, show)
+    plot_dedup(df.copy(), 3, save, show)
+    plot_pod_skew_logical_scatter(df.copy(), 1, save, show)
+    plot_pod_skew_logical_scatter(df.copy(), 2, save, show)
+    plot_pod_skew_logical_scatter(df.copy(), 3, save, show)
+    plot_pod_skew_physical_scatter(df.copy(), 1, save, show)
+    plot_pod_skew_physical_scatter(df.copy(), 2, save, show)
+    plot_pod_skew_physical_scatter(df.copy(), 3, save, show)
 
 
 def main():
-    summary_plots('combined_summary_stats.csv')
+    summary_plots('combined_summary_stats.csv', save=True, show=False)
 
 
 if __name__ == "__main__":
